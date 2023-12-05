7886ebfdccd81fc8c2970e97fb5fe408ab121501
roochieng <49204011+roochieng@users.noreply.github.com>
2023-12-05 17:25:24 +0000

    Write a method that determines if all the boxes can be opened.

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A method that determines if all the boxes can be opened.
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    This function will take a list of lists and the content
+       of a list will unlock other lists
+    """
+
+    keys = [0]
+    for key in keys:
+        for boxKey in boxes[key]:
+            if boxKey not in keys and boxKey < len(boxes):
+                keys.append(boxKey)
+    if len(keys) == len(boxes):
+        return True
+    return False
